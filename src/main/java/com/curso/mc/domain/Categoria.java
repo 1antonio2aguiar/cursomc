4464143a2197e269f39b0a6144459fec5fbf599c
Antonio Aguiar <antonio.oliveira@codiub.com.br>
2023-04-11 14:45:09 -0300

    Banco h2 e criacao da base de dados

---
+++ src/main/java/com/curso/mc/domain/Categoria.java
@@ -0,0 +1,28 @@
+package com.curso.mc.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@AllArgsConstructor
+@EqualsAndHashCode
+@Entity
+public class Categoria implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+	
+	public Categoria() {}
+	
+}
